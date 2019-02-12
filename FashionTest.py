61ae8a27abb2e02f4ef2101cf492c52fedfeef81
qinyuelong <qinyuelong.cn@gmail.com>
2019-02-12 17:32:17 +0800

    完成衣服鞋子分类项目练习

--- FashionTest.py
+++ FashionTest.py
@@ -72,3 +72,80 @@ predictions = model.predict(test_images)
 
 index = np.argmax(predictions[0])
 print("First image is :", class_names[index])
+
+
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array[i], true_label[i],img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{}{:2.0f}%({})".format(class_names[predicted_label], 100 * np.max(predictions_array), class_names[true_label]), color = color)
+
+
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array[i], true_label[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color = '#777777')
+    plt.ylim([0,1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+
+i = 0
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i,predictions,test_labels,test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions, test_labels)
+
+
+i = 12
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions, test_labels, test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions, test_labels)
+
+
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+    plt.subplot(num_rows, 2*num_cols, 2*i+1)
+    plot_image(i, predictions,test_labels, test_images)
+    plt.subplot(num_rows, 2*num_cols, 2*i+2)
+    plot_value_array(i,predictions, test_labels)
+
+
+
+
+# 最后，使用经过训练的模型对单个图像进行预测。
+
+img = test_images[0]
+print(img.shape)
+
+img = (np.expand_dims(img,0))
+print(img.shape)
+
+predictions_single = model.predict(img)
+print("predictions_single = :", predictions_single)
+print(np.argmax(predictions_single[0]))
+
+
+
+
+plt.show()
